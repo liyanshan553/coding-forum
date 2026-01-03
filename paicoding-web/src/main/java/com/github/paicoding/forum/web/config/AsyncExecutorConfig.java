382d3ef3b37bdefc4cf31bf70cd2037b7dd74167
李沿杉 <75842543+liyanshan553@users.noreply.github.com>
2026-01-03 13:53:35 +0800

    Configure TTL async executor

---
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/config/AsyncExecutorConfig.java
@@ -0,0 +1,31 @@
+package com.github.paicoding.forum.web.config;
+
+import com.alibaba.ttl.threadpool.TtlExecutors;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncExecutorConfig implements AsyncConfigurer {
+
+    @Bean(name = "asyncExecutor")
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() * 2);
+        executor.setMaxPoolSize(64);
+        executor.setQueueCapacity(0);
+        executor.setThreadNamePrefix("paicoding-async-");
+        executor.initialize();
+        return TtlExecutors.getTtlExecutor(executor);
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
+}
